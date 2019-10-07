e472bece1bba275ea26a985e1e2048d77e7b3985
gmd <maoyiluo@gmail.com>
2019-10-06 21:47:58 -0400

    set up the mysql

--- app/service/service.py
+++ app/service/service.py
@@ -23,36 +23,36 @@ class Payments(db.Model):
     def __repr__(self):
         return '<Payments %r>' % (self.name)
 
-    # def save(self):
-    #     """
-    #     Saves a Payments to the data store
-    #     """
-    #     Payments.logger.info('Saving %s', self.types)
-    #     if not self.id:
-    #         db.session.add(self)
-    #     db.session.commit()
+    def save(self):
+        """
+        Saves a Payments to the data store
+        """
+        Payments.logger.info('Saving %s', self.types)
+        if not self.id:
+            db.session.add(self)
+        db.session.commit()
 
-    # def delete(self):
-    #     """ Removes a Payments from the data store """
-    #     Payments.logger.info('Deleting %s', self.name)
-    #     db.session.delete(self)
-    #     db.session.commit()
+    def delete(self):
+        """ Removes a Payments from the data store """
+        Payments.logger.info('Deleting %s', self.name)
+        db.session.delete(self)
+        db.session.commit()
 
-    # def serialize(self):
-    #     """ Serializes a Payments into a dictionary """
-    #     return {"id": self.id,
-    #             "types": self.types,
-    #             "available": self.available}
+    def serialize(self):
+        """ Serializes a Payments into a dictionary """
+        return {"id": self.id,
+                "types": self.types,
+                "available": self.available}
 
-    # def deserialize(self, data):
-    #     """
-    #     Deserializes a Payments from a dictionary
-    #     Args:
-    #         data (dict): A dictionary containing the Payments data
-    #     """
-    #     self.types = data['types']
-    #     self.available = data['available']
-    #     return self
+    def deserialize(self, data):
+        """
+        Deserializes a Payments from a dictionary
+        Args:
+            data (dict): A dictionary containing the Payments data
+        """
+        self.types = data['types']
+        self.available = data['available']
+        return self
 
     @classmethod
     def init_db(cls, app):
@@ -64,11 +64,11 @@ class Payments(db.Model):
         app.app_context().push()
         db.create_all()  # make our sqlalchemy tables
 
-    # @classmethod
-    # def all(cls):
-    #     """ Returns all of the Payments in the database """
-    #     cls.logger.info('Processing all Pets')
-    #     return cls.query.all()
+    @classmethod
+    def all(cls):
+        """ Returns all of the Payments in the database """
+        cls.logger.info('Processing all Pets')
+        return cls.query.all()
 
 @app.route('/')
 def index():
@@ -80,7 +80,7 @@ if __name__ == "__main__":
         "types":"credit card",
         "available": True
     }
-    # payment = Payments()
-    # payment.deserialize(dummyPayment)
-    # payment.save()
+    payment = Payments()
+    payment.deserialize(dummyPayment)
+    payment.save()
     app.run()
