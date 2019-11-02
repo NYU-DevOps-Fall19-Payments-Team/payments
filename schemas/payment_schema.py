f8cb6020cdd18d87eb2e0dc4daa1b02714a98d2f
Zheng Jiang <zhengjiangdqr@gmail.com>
2019-11-02 02:30:55 -0400

    Fix payment schema

--- schemas/payment_schema.py
+++ schemas/payment_schema.py
@@ -1,108 +1,110 @@
 payment_schema = {
     "title": "payment",
     "type": "object",
-    "additionalProperties": False,
     "properties": {
         "order_id": {
             "type": "number",
-            "minimum": 1,
-            "required": True
+            "minimum": 1
         },
         "customer_id": {
             "type": "number",
-            "minimum": 1,
-            "required": True
+            "minimum": 1
         },
         "available": {
-            "type": "boolean",
-            "required": True
+            "type": "boolean"
         },
         "payments_type": {
             "type": "string",
-            "enum": ["credit card", "paypal"],
-            "required": True
-        },
-        "allOf": [
-            {
-                "if": {
-                    "properties": {
-                        "payments_type": {"const": "credit card"}
-                    }
-                },
-                "then": {
-                    "properties": {
-                        "info": {
-                            "type": "object",
-                            "required": True,
-                            "additionalProperties": False,
-                            "properties": {
-                                "credit_card_number": {
-                                    "type": "string",
-                                    "maxLength": 30,
-                                    "required": True
-                                },
-                                "card_holder_name": {
-                                    "type": "string",
-                                    "maxLength": 30,
-                                    "required": True
-                                },
-                                "expiration_month": {
-                                    "type": "number",
-                                    "minimum": 1,
-                                    "maximum": 12,
-                                    "required": True
-                                },
-                                "expiration_year": {
-                                    "type": "number",
-                                    "minimum": 2020,
-                                    "maximum": 2099,
-                                    "required": True
-                                },
-                                "security_code": {
-                                    "type": "string",
-                                    "maxLength": 4,
-                                    "required": True
-                                }
-                            }
-                        }
-                    }
+            "enum": ["credit card", "paypal"]
+        }
+    },
+    "allOf": [
+        {
+            "if": {
+                "properties": {
+                    "payments_type": {"const": "credit card"}
                 }
             },
-            {
-                "if": {
-                    "properties": {
-                        "payments_type": {"const": "paypal"}
-                    }
-                },
-                "then": {
-                    "properties": {
-                        "info": {
-                            "type": "object",
-                            "required": True,
-                            "additionalProperties": False,
-                            "properties": {
-                                "email": {
-                                    "type": "string",
-                                    "maxLength": 30,
-                                },
-                                "phone_number": {
-                                    "type": "string",
-                                    "maxLength": 20,
-                                },
-                                "token": {
-                                    "type": "string",
-                                    "maxLength": 30,
-                                    "required": True
-                                }
+            "then": {
+                "properties": {
+                    "info": {
+                        "type": "object",
+                        "additionalProperties": False,
+                        "properties": {
+                            "credit_card_number": {
+                                "type": "string",
+                                "maxLength": 30
                             },
-                            "anyOf": [
-                                {"required": ["email"]},
-                                {"required": ["phone_number"]}
-                            ]
-                        }
+                            "card_holder_name": {
+                                "type": "string",
+                                "maxLength": 30
+                            },
+                            "expiration_month": {
+                                "type": "number",
+                                "minimum": 1,
+                                "maximum": 12
+                            },
+                            "expiration_year": {
+                                "type": "number",
+                                "minimum": 2020,
+                                "maximum": 2099
+                            },
+                            "security_code": {
+                                "type": "string",
+                                "maxLength": 4
+                            }
+                        },
+                        "required": [
+                            "credit_card_number",
+                            "card_holder_name",
+                            "expiration_month",
+                            "expiration_year",
+                            "security_code"
+                        ]
                     }
                 }
             }
-        ]
-    }
+        },
+        {
+            "if": {
+                "properties": {
+                    "payments_type": {"const": "paypal"}
+                }
+            },
+            "then": {
+                "properties": {
+                    "info": {
+                        "type": "object",
+                        "additionalProperties": False,
+                        "properties": {
+                            "email": {
+                                "type": "string",
+                                "maxLength": 30,
+                            },
+                            "phone_number": {
+                                "type": "string",
+                                "maxLength": 20,
+                            },
+                            "token": {
+                                "type": "string",
+                                "maxLength": 30
+                            }
+                        },
+                        "anyOf": [
+                            {"required": ["email"]},
+                            {"required": ["phone_number"]}
+                        ],
+                        "required": ["token"]
+                    }
+                }
+            }
+        }
+    ],
+    "required": [
+        "order_id",
+        "customer_id",
+        "available",
+        "payments_type",
+        "info"
+    ]
 }
