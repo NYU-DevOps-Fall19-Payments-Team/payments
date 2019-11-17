8fc3480a91c1f26fbd25c97ecd22ee1246f25497
Amir Shirif <ams1671@nyu.edu>
2019-11-17 01:00:46 +0000

    first running test

---
+++ features/steps/payment_steps.py
@@ -0,0 +1,27 @@
+"""
+Pet Steps
+
+Steps file for Pet.feature
+
+For information on Waiting until elements are present in the HTML see:
+    https://selenium-python.readthedocs.io/waits.html
+"""
+from os import getenv
+import logging
+import json
+import requests
+from behave import *
+from compare import expect, ensure
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions
+
+WAIT_SECONDS = int(getenv('WAIT_SECONDS', '60'))
+
+@when('I visit the "home page"')
+def step_impl(context):
+    """ Make a call to the base URL """
+    context.driver.get(context.base_url)
+    # Uncomment next line to take a screenshot of the web page
+    #context.driver.save_screenshot('home_page.png')
