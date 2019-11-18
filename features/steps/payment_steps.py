19f81b580f067bf4f90064601a171175e18d3468
Amir Shirif <ams1671@nyu.edu>
2019-11-18 15:42:19 -0500

    pulled in working code from alt. branch

--- features/steps/payment_steps.py
+++ features/steps/payment_steps.py
@@ -40,7 +40,7 @@ def step_impl(context):
 def step_impl(context):
     """ Make a call to the base URL """
     context.driver.get(context.base_url)
-    context.driver.save_screenshot('home_page.png')
+    #context.driver.save_screenshot('home_page.png')
 
 @when('I press the "{button}" button')
 def step_impl(context, button):
