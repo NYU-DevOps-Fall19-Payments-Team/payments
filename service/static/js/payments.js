3a5b1dd01a4c4322675144aee95acf51c9825770
gmd <maoyiluo@gmail.com>
2019-11-17 21:06:48 -0500

    store the tempory stage

--- service/static/js/payments.js
+++ service/static/js/payments.js
@@ -1,6 +1,40 @@
 $(function () {
+    // load all the payments from the database, insert it into 
+    console.log("----------loading table------------")
+    var ajax = $.ajax({
+        type: "GET",
+        url: "/payments",
+        contentType: "application/json"
+    });
+
+    ajax.done(function(res){
+        for(i = 0; i < res.length; i++)
+            addRow(res[i])
+    });
+    
+    function addRow(payment){
+        let type = payment.type;
+        let id = payment.id;
+        let info;
+        switch (type){
+            case "credit card":
+                info = payment.info.credit_card_number;
+                $("#display_credit_card").append(`<div class='row'><div class='col'><i class=\"far fa-credit-card\"></i></div><div class='col-2'><p>${id}</p></div><div class='col'><p>${info}</p></div></div>`);
+                break;
+            case "paypal":
+                info = payment.info.email;
+                $("#display_paypal").append(`<div class='row'><div class='col'><i class="fab fa-cc-paypal"></i></div><div class='col-2'><p>${id}</p></div><div class='col'><p>${info}</p></div></div>`);
+                break;
+            default:
+                console.log(payment.type)
+        }
+    };
+
+    // create a new payment.
     $("#submit").click(function () {
+        // don't refresh the page.
         event.preventDefault();
+        // in case any field is empty, if so it will throw an error. The error will be caught by the catch block.
         try {
             let available = false;
             if ($("#available").val() == "Yes")
@@ -39,27 +73,33 @@ $(function () {
                 type: type,
                 info: info
             };
+            // send the data to database.
             var ajax = $.ajax({
                 type: "POST",
                 url: "/payments",
                 contentType: "application/json",
                 data: JSON.stringify(data),
             });
-
+            
+            // if the ajax request is succeed, add to the table and reset the form.
             ajax.done(function(res){
                 $("#payment_id").text(res.id);
                 $("#payment_type").text(res.type);
                 $("#payment_available").text(res.available);
+                addRow(res);
+                $("#submit").trigger('reset');
             });
-
+            
+            // if the ajax request is failed, show the error.
             ajax.fail(function (res) {
                 showError(res.responseJSON);
             });
-        } catch (err) {
+        }catch (err) {
             showError(err);
         }
     });
 
+    // Different forms will be shown base on the type of the payment.
     type.addEventListener('change', () => {
         let index = type.selectedIndex;
         switch (index) {
@@ -77,10 +117,12 @@ $(function () {
         }
     });
 
+    // Show the error message.
     function showError(error) {
         let errorMessage = $(".error");
         errorMessage.css("display", "block");
         errorMessage.text(error.message);
     }
+
 });
 
